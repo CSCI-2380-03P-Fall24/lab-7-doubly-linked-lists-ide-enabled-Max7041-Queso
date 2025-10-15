4ea4b621bd714e3a5a8a2edd6d104a3f389e525b
Max7041-Queso <jose.buena01@utrgv.edu>
2025-10-15 23:04:03 +0000

    WIP

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,18 +1,47 @@
 	#include "StudentList.h"
 
 	// Define a constructor to initialize the list. The list starts with no Students
-    StudentList::StudentList() {}
+   StudentList::StudentList() {
+    head = nullptr;
+    tail = nullptr;
+    numStudents = 0;
+}
 
 	// return the number of students currently in the list
 	int StudentList::listSize() {
-		return -1;
+		return numStudents;
 	}
 
 	//add a Node with a student to the front (head) of the list.
-	void StudentList::addFront(Student s) {}
-
+	void StudentList::addFront(Student s) {
+		if (head == nullptr){
+			Node* newNode = new Node(s);
+			head = newNode;
+			tail = newNode;
+		}
+		else if (numStudents >= 1){
+			Node* newNode = new Node(s);
+			newNode -> next = head;
+			head -> prev = newNode;
+			head = newNode;
+		}
+		numStudents++;
+	}
 	//add a Node with a student to the back (tail) of the list.
-	void StudentList::addBack(Student s) {}
+	void StudentList::addBack(Student s) {
+		if (head == nullptr){
+			Node* newNode = new Node(s);
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			Node* newNode = new Node(s);
+			tail -> next = newNode;
+			newNode -> prev = tail;
+			tail = newNode;
+		}
+		numStudents++;
+	}
 
 	//Print out the names of each student in the list.
 	void StudentList::printList() {}
